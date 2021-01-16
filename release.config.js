ab6147f55a62e1fac929311b8d58716340380fd2
Brad Garropy <bradgarropy@gmail.com>
2021-01-15 22:18:38 -0600

    ci(release): map commit types to release levels

--- release.config.js
+++ release.config.js
@@ -1,7 +1,21 @@
 const config = {
     "dryRun": false,
     "plugins": [
-        ["@semantic-release/commit-analyzer", {preset: "conventionalcommits"}],
+        ["@semantic-release/commit-analyzer", {
+            preset: "conventionalcommits",
+            releaseRules: [
+                {
+                    type: "feat", release: "minor",
+                    type: "fix", release: "patch",
+                    type: "docs", release: "patch",
+                    type: "style", release: "patch",
+                    type: "refactor", release: "patch",
+                    type: "perf", release: "patch",
+                    type: "test", release: "patch",
+                    type: "chore", release: "patch",
+                }
+            ]
+        }],
         "@semantic-release/release-notes-generator",
         "@semantic-release/npm",
         "@semantic-release/github"
