3d5ddb4e7f49b331028bef80237d14248d429525
Brad Garropy <bradgarropy@gmail.com>
2021-01-17 10:20:59 -0600

    ci(release): configure types to include in the release notes

--- release.config.js
+++ release.config.js
@@ -1,31 +1,43 @@
 const config = {
     "dryRun": false,
     "plugins": [
-        ["@semantic-release/commit-analyzer", {
-            preset: "conventionalcommits",
-            releaseRules: [
-                {type: "feat", release: "minor"},
-                {type: "fix", release: "patch"},
-                {type: "docs", release: "patch"},
-                {type: "style", release: "patch"},
-                {type: "refactor", release: "patch"},
-                {type: "perf", release: "patch"},
-                {type: "test", release: "patch"},
-                {type: "build", release: "patch"},
-                {type: "ci", release: "patch"},
-                {type: "chore", release: "patch"},
-                {type: "revert", release: "patch"},
-            ]
-        }],
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                preset: "conventionalcommits",
+                releaseRules: [
+                    {type: "feat", release: "minor"},
+                    {type: "fix", release: "patch"},
+                    {type: "docs", release: "patch"},
+                    {type: "style", release: "patch"},
+                    {type: "refactor", release: "patch"},
+                    {type: "perf", release: "patch"},
+                    {type: "test", release: "patch"},
+                    {type: "build", release: "patch"},
+                    {type: "ci", release: "patch"},
+                    {type: "chore", release: "patch"},
+                    {type: "revert", release: "patch"},
+                ]
+            }
+        ],
         [
             "@semantic-release/release-notes-generator",
             {
                 "preset": "conventionalcommits",
-                "parserOpts": {
-                    "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
-                },
-                "writerOpts": {
-                    "commitsSort": ["subject", "scope"]
+                "presetConfig": {
+                    "types": [
+                        {"type": "feat", "section": "Features"},
+                        {"type": "fix", "section": "Bug Fixes"},
+                        {"type": "docs", "hidden": false},
+                        {"type": "style", "hidden": false},
+                        {"type": "refactor", "hidden": false},
+                        {"type": "perf", "hidden": false},
+                        {"type": "test", "hidden": false},
+                        {"type": "build", "hidden": false},
+                        {"type": "ci", "hidden": false},
+                        {"type": "chore", "hidden": false},
+                        {"type": "revent", "hidden": false},
+                    ]
                 }
             }
         ],
@@ -37,7 +49,8 @@ const config = {
         ],
         "@semantic-release/npm",
         [
-            "@semantic-release/git", {
+            "@semantic-release/git",
+            {
                 "assets": ["changelog.md", "package.json", "package-lock.json"],
                 "message": "chore(release): ${nextRelease.version} [skip ci]"
             }
